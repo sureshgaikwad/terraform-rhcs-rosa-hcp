fdf0b50641a462e8f328db8686a11ff3e1da8b41
sureshgaikwad <sgaikwad@redhat.com>
2025-08-06 16:50:08 +0530

    auto

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ locals {
 provider "kubernetes" {
   config_path = module.rosa_cluster_hcp.kubeconfig_path
 }
+
+module "vpc" {
+  source = "./modules/vpc"
+
+  name_prefix              = var.cluster_name
+  availability_zones_count = 3
+}
+
+
 ############################
 # Cluster
 ############################
@@ -14,18 +23,19 @@ module "hcp" {
   cluster_name             = var.cluster_name
   openshift_version        = var.openshift_version
   machine_cidr             = module.vpc.cidr_block
-  aws_subnet_ids           = concat(module.vpc.public_subnet, module.vpc.private_subnet)
+  aws_subnet_ids           = concat(module.vpc.public_subnets, module.vpc.private_subnets)
   aws_availability_zones   = module.vpc.availability_zones
   replicas                 = length(module.vpc.availability_zones)
   create_admin_user        = true
   ec2_metadata_http_tokens = "required"
-#aws_subnet_ids = aws_subnet.rosa_subnets[*].id
   // STS configuration
   create_account_roles  = true
   account_role_prefix   = local.account_role_prefix
   create_oidc           = true
   create_operator_roles = true
   operator_role_prefix  = local.operator_role_prefix
+  
+  depends_on = [module.vpc]
 }
 
 ############################
@@ -49,12 +59,3 @@ resource "random_password" "password" {
   min_upper = 1
 }
 
-############################
-# VPC
-############################
-module "vpc" {
-  source = "./modules/vpc"
-
-  name_prefix              = var.cluster_name
-  availability_zones_count = 3
-}
